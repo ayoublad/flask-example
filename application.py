3efaafe43c1854665bd3f169805aee9ecaf529eb
ayoublad <ayoub.ladjici@outlook.com>
2024-07-03 11:40:15 +0200

    Mise a jour

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Je suis en stage chez Capgemini'
+    return 'Je suis en stage chez Capgemini. [MAJ] Cela fait plus de deux semaines.'
